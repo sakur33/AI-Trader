481e07137f8ddd0ff1c83052b406afa6ccd0044f
Alejandro <alejandro.barredo@tecnalia.com>
2022-12-23 08:42:33 +0100

    adding sqlite

--- src/trading_accounts.py
+++ src/trading_accounts.py
@@ -1,9 +1,11 @@
 import os
 import pandas as pd
 from utils import *
-from XTB_WS_CLIENT import *
+import sqlite3
+from xAPIConnector import *
 
 today = get_today()
+today_str = get_today_ms
 curr_path = os.path.dirname(os.path.realpath(__file__))
 data_path = curr_path + "../../data/"
 symbol_path = curr_path + "../../symbols/"
@@ -11,42 +13,31 @@ cluster_path = curr_path + "../../clusters/"
 model_path = curr_path + "../../model/"
 result_path = curr_path + "../../result/"
 docs_path = curr_path + "../../docs/"
+database_path = curr_path + "../../database/"
 
 
 class trader:
-    def __init__(self, capital, max_risk, trade_type) -> None:
+    def __init__(self, name, capital, max_risk, trade_type) -> None:
+        self.name = name
         self.capital = capital
         self.max_risk = max_risk
-        self.trade_type = trade_type
-        self.invested_capital = 0
-        self.active_trades = pd.DataFrame(
-            data=[],
-            columns=[
-                "Date",
-                "symbol",
-                "amount",
-                "buy_price",
-                "stop_loss",
-                "take_profit",
-            ],
-        )
-        self.historic_trades = pd.DataFrame(
-            data=[],
-            columns=[
-                "Type",
-                "Date_entry",
-                "Date_close",
-                "symbol",
-                "amount",
-                "buy_price",
-                "initial_stop_loss",
-                "initial_take_profit",
-                "stop_loss_trail",
-                "take_profit_trail",
-            ],
-        )
+        db_conn = self.client("ai_trader.db")
+        self.stream_client = APIStreamClient()
+        self.client = APIClient()
 
     def make_trade(self, symbol_info, current_price, stop_loss=None, take_profit=None):
-        symbol_info = get_symbol(symbol)
+        # TODO: 
 
-        print()
+    def create_connection(self, db_file):
+        conn = None
+        try:
+            conn = sqlite3.connect(f"{database_path}{db_file}")
+        except Exception as e:
+            print(e)
+        return conn
+
+    def insert_trader(self, conn):
+        sql = f"INSERT INTO traders (tradername, creation_date, capital, max_risk, profits, losses) VALUES ({self.name},'{today}',{self.capital},{self.max_risk},0, 0);"
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
