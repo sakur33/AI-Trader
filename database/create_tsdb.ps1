64287afb4478fdce513b4cc01cf4d746ae59d632
alejandro.barredo <alejandro.barredo@kurago.software>
2023-01-02 10:04:42 +0100

    create tick db script

--- database/create_tsdb.ps1
+++ database/create_tsdb.ps1
@@ -1,8 +1,9 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
 Write-Output "Installing Docker"
-if (Test-Path -Path "C:\'Program Files'\Docker\")
-    {
+if (Test-Path -Path "C:\Program Files\Docker")
+    {   
+        Invoke-Expression "C:\Program Files\Docker\Docker\Docker Desktop.exe"
         $dockerv = Invoke-Expression "docker --version"
         Write-Output "Docker already installed: $dockerv"
     }
@@ -11,7 +12,17 @@ if (Test-Path -Path "C:\'Program Files'\Docker\")
         Invoke-WebRequest -UseBasicParsing -Uri "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe" -OutFile "./temp/docker.exe";
         Start-Process ".\temp\docker.exe"
     }
-    Invoke-Expression "docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=1234 --name tick_db timescale/timescaledb-ha:latest-pg14"        
+      
+
+Write-Output "Creating tick_db"
+if (-not (docker ps -a | Select-String -Pattern 'tick_db' -Quiet))
+    {  
+        Invoke-Expression "docker pull timescale/timescaledb-ha:pg14-latest"
+        Invoke-Expression "docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=1234 --name tick_db timescale/timescaledb-ha:pg14-latest"  
+    }
+    else {
+        Write-Output "tick_db already created"
+    }
 
 Write-Output "Installing DBeaver"
 if (Test-Path -Path "C:\Program Files\DBeaver")
