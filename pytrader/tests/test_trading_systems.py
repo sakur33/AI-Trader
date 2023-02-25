8b62f6a6531ecaa0de820150760bfd8e0a84af7f
Alejandro <alejandro.barredo@tecnalia.com>
2023-02-25 08:19:55 +0100

    Added visualiztor for future graphing system

--- pytrader/tests/test_trading_systems.py
+++
@@ -1,9 +0,0 @@
-from pytrader.src import custom_exceptions
-from pytrader.src import trading_systems
-
-
-def test_trader_bad_credentials() -> None:
-    try:
-        trader = Trader(user="", passwd="")
-    except Exception as e:
-        assert isinstance(e, LoginError)
